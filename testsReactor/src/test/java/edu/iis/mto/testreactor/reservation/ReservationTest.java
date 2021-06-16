a1fe00a9b61f5059f0a2048295175681172aa6a4
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-06-16 17:43:43 +0200

    prototyte tests

--- testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -1,7 +1,34 @@
 package edu.iis.mto.testreactor.reservation;
 
+import edu.iis.mto.testreactor.offer.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
+    @Mock
+    Reservation reservation;
+    @Mock
+    DiscountPolicy discountPolicy;
+
+    Reservation.ReservationStatus reservationStatus;
+    List<ReservationItem> items = List.of(ReservationItem(Product(new Id(),)));
+
+
+    @Test
+    void ifdiscountPolicyIsNull()
+    {
+        fail();
+    }
+    @Test
+    void ifListItemIsEmptyShoudReturnEmppyOffer()
+    {
+        
+    }
+
+
 
 }
