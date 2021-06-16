330d9fffb2a3efd10283c08000fe8163d86bf584
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-06-16 18:07:48 +0200

    clean code

--- testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.testreactor.reservation;
 import edu.iis.mto.testreactor.money.Money;
 import edu.iis.mto.testreactor.offer.DiscountPolicy;
 import edu.iis.mto.testreactor.offer.Offer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
@@ -37,6 +38,13 @@ class ReservationTest {
         reservation = new Reservation(id, Reservation.ReservationStatus.OPENED,clientData,date);
     }
     @Test
+    void ConstrucotrIsWork() throws ParseException {
+        assertEquals(clientData, reservation.getClientData());
+        assertEquals(date, reservation.getCreateDate());
+        assertEquals( new ArrayList<>(), reservation.getReservedProducts());
+        assertEquals( Reservation.ReservationStatus.OPENED, reservation.getStatus());
+    }
+    @Test
     void emptyOffer() throws ParseException {
         Offer return_offer = reservation.calculateOffer(discountPolicy);
         assertEquals(new Offer(new ArrayList<>(), new ArrayList<>()), return_offer);
@@ -47,21 +55,12 @@ class ReservationTest {
     void shouldInvokeIsClosedOneceWhenAdding() {
 
     }
-
-    @Test
-    void ifdiscountPolicyIsNull()
-    {
-        fail();
-    }
-    @Test
-    void ifListItemIsEmptyShoudReturnEmppyOffer()
-    {
-
-    }
     @Test
     void ifProductIsAvailableShouldApplyDiscount()
     {
 
+        doReturn(true).when(reservation.).termalCircuit(any(HeatingSettings.class));
+        Assertions.assertThrows(OvenException.class, () -> oven.start(bakingProgram));
     }
     @Test
     void ifProductIsNotAvailableShouldApplyDiscount()
