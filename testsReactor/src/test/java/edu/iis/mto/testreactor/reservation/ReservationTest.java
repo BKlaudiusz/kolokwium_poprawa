267e03ee644a6e70152ecf8f227245e3f0a6749e
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-06-16 17:54:38 +0200

    protoryte test and first test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -1,12 +1,16 @@
 package edu.iis.mto.testreactor.reservation;
 
+import edu.iis.mto.testreactor.money.Money;
 import edu.iis.mto.testreactor.offer.DiscountPolicy;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 class ReservationTest {
     @Mock
@@ -15,8 +19,20 @@ class ReservationTest {
     DiscountPolicy discountPolicy;
 
     Reservation.ReservationStatus reservationStatus;
-    List<ReservationItem> items = List.of(ReservationItem(Product(new Id(),)));
 
+
+    @Test
+    void shouldInvokeIsClosedOneceWhenAdding() {
+
+        Money money = new Money(100, "EUR");
+        Product product = new Product(Id.generate(), money, "Tshirt",ProductType.STANDARD);
+
+        reservation.add(product, 1);
+
+        verify(reservation, times(1)).isClosed();
+        InOrder in = inOrder(reservation);
+        in.verify(reservation).isClosed();
+    }
 
     @Test
     void ifdiscountPolicyIsNull()
@@ -26,8 +42,33 @@ class ReservationTest {
     @Test
     void ifListItemIsEmptyShoudReturnEmppyOffer()
     {
-        
+
     }
+    @Test
+    void ifProductIsAvailableShouldApplyDiscount()
+    {
+
+    }
+    @Test
+    void ifProductIsNotAvailableShouldApplyDiscount()
+    {
+
+    }
+    @Test
+    void itShoudlThrowErrorWhenTryinToClosedReservation()
+    {
+
+    }
+    @Test
+    void ShouldThrowExeptionWhenTrying()
+    {
+
+    }
+
+
+
+
+
 
 
 
